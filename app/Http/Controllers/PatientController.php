59531f013a543b926e61fef1991188c8f208eb9e
Narendra <narendra.negara@glovory.com>
2022-07-21 22:16:05 +0700

    [ADD] Pasien form

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -12,21 +12,29 @@ use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-  public function index()
-  {
-      $id = Auth::user()->id;
-      $response = Response::where('response_user_id', $id)->get();
-      return view('pasien/dashboard-pasien', compact('response'));
-  }
+    public function index()
+    {
+        $id = Auth::user()->id;
+        $response = Response::where('response_user_id', $id)->get();
+        return view('pasien/dashboard-pasien', compact('response'));
+    }
 
-  public function create()
-  {
-      $id = Auth::user()->id;
-      $questions = Form::where('forms.id', 1)
-          ->join('question_segments', 'question_segments.form_id', '=', 'forms.id')
-          ->join('questions', 'questions.question_segment_id', '=', 'question_segments.id')->get();
-          //->leftJoin('choices', 'choices.question_id', '=', 'questions.id')-
-      dump($questions);
-      return view('pasien/form-keluhan', compact('questions'));
-  }
+    public function create()
+    {
+        $id = Auth::user()->id;
+        $segments = Form::where('forms.id', 1)
+        ->join('question_segments', 'question_segments.form_id', '=', 'forms.id')->get();
+
+        $questions = Form::where('forms.id', 1)
+            ->join('question_segments', 'question_segments.form_id', '=', 'forms.id')
+            ->join('questions', 'questions.question_segment_id', '=', 'question_segments.id')->get();
+
+        $choices = Form::where('forms.id', 1)
+            ->join('question_segments', 'question_segments.form_id', '=', 'forms.id')
+            ->join('questions', 'questions.question_segment_id', '=', 'question_segments.id')
+            ->leftJoin('choices', 'choices.question_id', '=', 'questions.id')->get();
+
+        // dump($segments, $questions, $choices);
+        return view('pasien/form-keluhan', compact('segments', 'questions', 'choices'));
+    }
 }
--- resources/views/pasien/form-keluhan.blade.php
+++ resources/views/pasien/form-keluhan.blade.php
@@ -8,6 +8,7 @@
             <div class="card-header pb-0 px-3">
                 <h6 class="mb-0">{{ __('Formulir Screening') }}</h6>
             </div>
+            <hr>
             <div class="card-body pt-4 p-3">
                 <form action="/user-profile" method="POST" role="form text-left">
                     @csrf
@@ -21,22 +22,85 @@
                         </div>
                     @endif
                     <div class="row">
-                        @foreach ($questions as $q)
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="user-name" class="form-control-label">{{ $q->question_detail }}</label>
-                                    <div class="@error('user.name')border border-danger rounded-3 @enderror">
-                                        <input class="form-control" value="{{ auth()->user()->name }}" type="text" placeholder="Name" id="user-name" name="name">
-                                            @error('name')
-                                                <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                                            @enderror
+                        @foreach ($segments as $s)
+                            <p class="font-weight-bolder">{{$s->question_segment}}</p>
+                            @foreach ($questions as $key => $q)
+                                @if ($s->id === $q->question_segment_id)
+                                    <div class="{{$q->question_type === 'options' ? 'col-12' : 'col-md-6'}}">
+                                        <div class="form-group">
+                                            <label class="form-control-label">{{$key + 1}}. {{ $q->question_detail }}</label>
+
+                                            {{--!!! Input !!!--}}
+                                            @if ($q->question_type == 'text')
+                                                <div class="@error('user.name')border border-danger rounded-3 @enderror">
+                                                    <input class="form-control" value="" type="text" placeholder="Silahkan diisi" id="input_{{$q->id}}" name="{{$q->id}}">
+                                                    @error('name')
+                                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                                    @enderror
+                                                </div>
+                                            @endif
+
+                                            {{--!!! Textarea !!!--}}
+                                            @if ($q->question_type == 'textarea')
+                                                <div class="@error('user.name')border border-danger rounded-3 @enderror">
+                                                    <input class="form-control" value="" row="3" type="textarea" placeholder="Silahkan diisi" id="textarea_{{$q->id}}" name="{{$q->id}}">
+                                                    @error('name')
+                                                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                                    @enderror
+                                                </div>
+                                            @endif
+
+                                            {{--!!! Radio !!!--}}
+                                            @if ($q->question_type == 'boolean')
+                                                <div class="row">
+                                                @foreach ($choices as $c)
+                                                    @if ($q->id === $c->question_id)
+                                                        <div class="col-2">
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" name="{{$q->id}}" id="radio_{{$q->id}}_{{$c->id}}">
+                                                                <label class="custom-control-label" for="radio_{{$q->id}}_{{$c->id}}">{{$c->choice}}</label>
+                                                            </div>
+                                                        </div>
+                                                    @endif
+                                                @endforeach
+                                                </div>
+                                            @endif
+
+                                            {{--!!! Checkbox !!!--}}
+                                            @if ($q->question_type == 'options')
+                                                <div class="row">
+                                                @foreach ($choices as $c)
+                                                    @if ($q->id === $c->question_id)
+                                                        <div class="col-4">
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="checkbox" value="{{$c->choice}}" id="options_{{$q->id}}_{{$c->id}}">
+                                                                <label class="custom-control-label" for="options_{{$q->id}}_{{$c->id}}">{{$c->choice}}</label>
+                                                            </div>
+                                                        </div>
+                                                    @endif
+                                                @endforeach
+                                                </div>
+                                            @endif
+
+                                            {{--!!! Checkbox !!!--}}
+                                            @if ($q->question_type == 'file')
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="file" >
+                                                </div>
+                                            @endif
+
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
+                                @endif
+                                @endforeach
+                            <hr>
                         @endforeach
+                        {{-- @foreach ($questions as $q)
+
+                        @endforeach --}}
                     </div>
                     <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn bg-gradient-dark btn-md mt-4 mb-4">{{ 'Save Changes' }}</button>
+                        <button type="submit" class="btn bg-gradient-dark btn-md mt-4 mb-4">{{ 'Kirim Keluhan' }}</button>
                     </div>
                 </form>
 
