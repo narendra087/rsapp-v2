102c22c211d84aabb7a25df7c4a3baff17a4360b
PrasastiKarunia <prasasti.karunia@gmail.com>
2022-07-24 22:14:10 +0700

    add controller dokter

--- app/Http/Controllers/DokterController.php
+++ app/Http/Controllers/DokterController.php
@@ -18,14 +18,60 @@ class DokterController extends Controller
         $response = Response::where('response_user_id', $id)->get();
         return view('dokter/dashboard-dokter', compact('response'));
     }
-    public function create(Request $request)
+    public function create($id)
     {
         $id = Auth::user()->id;
-        // $response = Response::where('response_user_id', $request->get('id'))
-        //     ->join('answers','answers.answer_response_id','=','responses.id')->get();
-        $questions = Form::where('forms.id', 3)
+        $userId = Auth::user()->id;
+        $response = Response::where('id', $id)->where('response_user_id', $userId)->get();
+        if (!count($response)) {
+            return redirect('/dashboard-pasien')->withErrors(['error' => 'You don`t have permissions']);
+        }
+
+        $answers = Answer::where('answer_response_id', $id)
+            ->leftJoin('choices', 'choices.id', '=', 'answers.answer_choice_id')->get();
+
+        $questions = Form::where('forms.id', 1)
             ->join('question_segments', 'question_segments.form_id', '=', 'forms.id')
             ->join('questions', 'questions.question_segment_id', '=', 'question_segments.id')->get();
-        return view('dokter/form-diagnosa', compact('questions'));
+
+        $questionsDokter = Form::where('forms.id', 3)
+            ->join('question_segments', 'question_segments.form_id', '=', 'forms.id')
+            ->join('questions', 'questions.question_segment_id', '=', 'question_segments.id')->get();
+
+        $data = array();
+        foreach ($questions as $key => $q) {
+            $description = $q->question_detail;
+
+            if ($q->question_type == 'options') {
+                $answer = array();
+            } else {
+                $answer = '';
+            }
+
+            foreach ($answers as $key => $a) {
+                if ($q->id == $a->answer_question_id) {
+                    if ($q->question_type == 'options') {
+                        array_push($answer, $a->choice);
+                    } else if ($q->question_type == 'boolean') {
+                        $answer = $a->choice;
+                    } else {
+                        $answer = $a->answer;
+                    }
+                }
+            }
+
+            if ($q->question_type == 'options') {
+                $answer = join(", ", $answer);
+            }
+
+            $data[] = [
+                'pertanyaan' => $description,
+                'jawaban' => $answer
+            ];
+        }
+
+        $diagnosa = '';
+        
+        return view('dokter/form-diagnosa', compact('response', 'data', 'diagnosa','questionDokter'));
     }
 }
--- resources/views/dokter/form-diagnosa.blade.php
+++ resources/views/dokter/form-diagnosa.blade.php
@@ -2,6 +2,51 @@
 
 @section('content')
 
+<div class="container-fluid py-4">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header pb-0 px-3">
+                    <h6 class="mb-0">Detail Keluhan</h6>
+                </div>
+                <hr>
+                <div class="px-3">
+                    <p class="m-0">Tanggal Keluhan: <span class="font-weight-bold">12-04-22</span></p>
+                </div>
+                <div class="card-body pt-4 p-3">
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                        <div class="d-flex flex-column">
+                            <h6 class="mb-3 text-sm">Data Keluhan</h6>
+                            <div class="row">
+                                @foreach ($data as $key => $dt)
+                                    <span class="mb-2 text-sm">{{$key + 1}}. {{$dt['pertanyaan']}}: <span class="text-dark font-weight-bold ms-sm-2">{{$dt['jawaban'] ? $dt['jawaban'] : '-'}}</span></span>
+                                @endforeach
+                            </div>
+                        </div>
+                    </li>
+                </div>
+                <hr>
+                <div class="px-3">
+                    <p class="m-0">Tanggal Diagnosa: <span class="font-weight-bold">-</span></p>
+                </div>
+                <div class="card-body pt-4 p-3">
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                        <div class="d-flex flex-column">
+                            <h6 class="mb-3 text-sm">Data Diagnosa</h6>
+                            <div class="row">
+                                @if ($diagnosa)
+                                    <span class="mb-2 text-sm">Diagnosa: <span class="text-dark font-weight-bold ms-sm-2">Hasil diagnosis disini</span></span>
+                                @else
+                                    <span class="mb-2 text-sm">Belum ada diagnosa.</span>
+                                @endif
+                            </div>
+                        </div>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div>
     <div class="container-fluid">
         <div class="card">
